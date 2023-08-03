d6af3eb813348417da24a65acb43a408ed653d85
Khabibulloev Mukhammadsodik <86304910+kmsmlengineer@users.noreply.github.com>
2023-08-03 17:17:02 +0300

    Update example_questions.py

---
+++ Day 1/example_questions.py
@@ -0,0 +1,13 @@
+# Question 1
+print('Hello world!')
+
+# Question 2
+a = input("Введите первое число: ")
+b = input("Введите второе число: ")
+c = a + b
+print(f"The sum of a and b: {c}")
+
+# Question 3
+t_Celsius = float(input("Введите температуру в Цельсиях: "))
+t_Fahrenheit = (t_Celsius*(9/5))+32
+print(f"The temperature in Fahrenheit: {t_Fahrenheit}")
